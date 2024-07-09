901e2f16faa7d6e5c3b8253ff65e00bfa386cb2f
Yaakov <yaakov.smith@wisetechglobal.com>
2024-07-09 14:11:12 +1000

    add detail to output

--- repro.ps1
+++ repro.ps1
@@ -1,10 +1,29 @@
 cd bin
 
+Write-Host
+Write-Host Building project...
 dotnet build ..\repro.sln
+
+Write-Host
+Write-Host Running with Assembly.Load...
 .\PluginHost.exe AssemblyLoad "RemoteWebAdministrationPlugin.ListRemoteIisSitesPlugin, RemoteWebAdministrationPlugin" $env:ComputerName
+
+Write-Host
+Write-Host Running with AssemblyLoadContext...
 .\PluginHost.exe AssemblyLoadContext "RemoteWebAdministrationPlugin.ListRemoteIisSitesPlugin, RemoteWebAdministrationPlugin" $env:ComputerName
 
+Write-Host
+Write-Host Rebuilding project with ProjectReference...
 dotnet build ..\repro.sln /p:AllowDirectReferences=true
+
+Write-Host
+Write-Host Running with Assembly.Load...
 .\PluginHost.exe AssemblyLoad "RemoteWebAdministrationPlugin.ListRemoteIisSitesPlugin, RemoteWebAdministrationPlugin" $env:ComputerName
+
+Write-Host
+Write-Host Running with AssemblyLoadContext...
 .\PluginHost.exe AssemblyLoadContext "RemoteWebAdministrationPlugin.ListRemoteIisSitesPlugin, RemoteWebAdministrationPlugin" $env:ComputerName
+
+Write-Host
+Write-Host Running with direct Type.GetType...
 .\PluginHost.exe Direct "RemoteWebAdministrationPlugin.ListRemoteIisSitesPlugin, RemoteWebAdministrationPlugin" $env:ComputerName
